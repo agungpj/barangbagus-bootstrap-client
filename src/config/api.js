e6f293ca749029c23d4fe192b8c308f3ad4141a0
Agung Prasetya <agungprasetya1121@gmail.com>
2022-06-22 18:49:48 +0700

    ready to deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,19 +1,13 @@
-export const API = () => {
-  const baseUrl =
-    process.env.REACT_APP_SERVER_URL ||
-    "https://dumbmerch-b34-agungpj.herokuapp.com/api/v1" ||
-    "https://localhost:5000/api/v1";
+import axios from "axios";
 
-  const executeAPI = async (endpoint, config) => {
-    const response = await fetch(baseUrl + endpoint, config);
-    const data = await response.json();
-    return data;
-  };
+export const API = axios.create({
+  baseURL: "http://localhost:5000/api/v1/",
+});
 
-  return {
-    get: executeAPI,
-    post: executeAPI,
-    patch: executeAPI,
-    delete: executeAPI,
-  };
+export const setAuthToken = (token) => {
+  if (token) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.commin["Authorization"];
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
 import React from "react";
+import App from "./components/App";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+
+import {
+  AlertProvider,
+  LoginProvider,
+  AdminProvider,
+} from "./contexts/AuthContext";
+import { UserProvider } from "./contexts/UserContext";
+import {
+  OrderProvider,
+  ProcessOrderProvider,
+  SuccessOrderProvider,
+} from "./contexts/OrderContext";
+import { ProfileProvider } from "./contexts/ProfileContext";
+
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import { UserContextProvider } from "./context/userContext";
-
-// Init Client
-const client = new QueryClient();
+import { TransactionProvider } from "./contexts/TransactionContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContextProvider>
-      <QueryClientProvider client={client}>
-        <Router>
-          <App />
-        </Router>
-      </QueryClientProvider>
-    </UserContextProvider>
+    <UserProvider>
+      <AdminProvider>
+        <LoginProvider>
+          <ProfileProvider>
+            <AlertProvider>
+              <TransactionProvider>
+                <OrderProvider>
+                  <SuccessOrderProvider>
+                    <ProcessOrderProvider>
+                      <Router>
+                        <App />
+                      </Router>
+                    </ProcessOrderProvider>
+                  </SuccessOrderProvider>
+                </OrderProvider>
+              </TransactionProvider>
+            </AlertProvider>
+          </ProfileProvider>
+        </LoginProvider>
+      </AdminProvider>
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
